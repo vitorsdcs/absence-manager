3f3be59fb8d9449c577e2684a8fe975ae73d3d9f
vitorsdcs <vitorsdcs@outlook.com>
2018-10-12 08:24:26 -0300

    Added filter by user and date

--- ruby_edition/main.rb
+++ ruby_edition/main.rb
@@ -3,5 +3,9 @@ require 'sinatra/reloader'
 require_relative './cm_challenge/api'
 
 get '/' do
-    p CmChallenge::Api.absences_with_members().to_json
+    user_id = params[:userId]
+    start_date = params[:startDate]
+    end_date = params[:endDate]
+
+    p CmChallenge::Api.absences_with_members(user_id, start_date, end_date).to_json
 end
