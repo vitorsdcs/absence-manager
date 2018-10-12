d4f8bbf70669ab5b12614c2af8f5786da970a417
vitorsdcs <vitorsdcs@outlook.com>
2018-10-12 08:36:00 -0300

    Downloading of absences as icalendar file

--- ruby_edition/cm_challenge/absences.rb
+++ ruby_edition/cm_challenge/absences.rb
@@ -1,9 +1,32 @@
+require 'icalendar'
 require_relative './api'
 
 module CmChallenge
   class Absences
-    def to_ical
-      # Your implementation here
+    def to_ical(user_id = nil, start_date = nil, end_date = nil)
+      cal = Icalendar::Calendar.new
+
+      Api.absences_with_members(user_id, start_date, end_date).map do |absence|
+        cal.event do |e|
+          e.dtstart     = DateTime.parse(absence[:start_date] + 'T00:00:00')
+          e.dtend       = DateTime.parse(absence[:end_date] + 'T23:59:59')
+          e.summary     = "#{absence[:member][:name]} is #{get_reason(absence[:type])}"
+          e.description = absence[:member_note]
+        end
+      end
+
+      cal.to_ical
+    end
+
+    def get_reason(type)
+      case type
+      when 'sickness'
+        'sick'
+      when 'vacation'
+        'on vacation'
+      else
+        'absent'
+      end
     end
   end
 end
--- ruby_edition/main.rb
+++ ruby_edition/main.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require_relative './cm_challenge/api'
+require_relative './cm_challenge/absences'
 
 get '/' do
     user_id = params[:userId]
     start_date = params[:startDate]
     end_date = params[:endDate]
 
-    p CmChallenge::Api.absences_with_members(user_id, start_date, end_date).to_json
+    content_type 'text/calendar'
+    attachment 'absences.ics'
+    absences = CmChallenge::Absences.new
+    absences.to_ical(user_id, start_date, end_date)
 end
